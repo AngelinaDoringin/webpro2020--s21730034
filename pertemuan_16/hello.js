93e9d5dc4348c4c4205bad10cb8d03364dcae8a3
AngelinaDoringin <s21730034@student.unklab.ac.id>
2020-11-10 09:06:31 +0700

    add pertemuan 16

---
+++ pertemuan_16/hello.js
@@ -0,0 +1,13 @@
+// export const numbers = [1,2,3,4,5];
+
+// export const world = (user) => {
+//    console.log ('Hello World');
+// }
+
+ const hello = (user) => {
+    console.log ('Hello, ${user}');
+}
+export default hello;
+
+
+//sir, tapunya error mar nintau dimana:D
