8f2a2f68a872d22da2a050dffa740ccf54442244
AngelinaDoringin <s21730034@student.unklab.ac.id>
2020-11-12 10:07:31 +0700

    add pertemuan 19

---
+++ pertemuan_19/index.js
@@ -0,0 +1,95 @@
+console.log ("JS dan class dan OOP");
+
+// Class and Constructor
+
+//const Person ={
+//    firstName: "Angel",
+//    lastName: "Doringin",
+//    displayFullName : function (){
+//        console.log(`${this.firstName} ${this.lastName}`);
+//    },
+//};
+//    //person.displayfullName();
+//    class Person {
+//    constructor(firstName, lastName){
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//    displayFullName(){
+//       console.log(`${this.firstName} ${this.lastName}`);
+//   }
+//}
+    //const person1 = new Person("Angel", "Doringin");
+    //console.log(person1);
+    //person1.displayFullName();
+    //const person2 = new Person("Chelsea", "Wowor");
+    //person2.displayFullName();
+
+
+// Inheritance
+
+
+//class student extends Person{
+//    constructor(firstName, lastName,NIM){
+//        super(firstName, LastName);
+//        this.NIM =NIM;
+//    }
+//    displayNIM()
+//    {
+//        console.log (`Your NIM is $(this,NIM)`);
+//    }
+//}
+//const angel = new Student ("Angel", "Doringin", "1050114858")
+//angel.displayFullName();
+//angel.displayNIM();
+
+
+// Static Member
+
+//class student extends Person{
+//    constructor(firstName, lastName,NIM, age){
+//        super(firstName, LastName);
+//        this.NIM = NIM;
+//        this.age = age;
+//    }
+//    static halo(){// static dianggap bukan function, properti didalamnya tidak bisa dipakai
+//        console.log (`halo `);
+//    }
+//    displayNIM()
+//    {
+//        console.log (`Your NIM is $(this,NIM)`);
+//    }
+//}
+//const john = new Student ("John", "Doe", "112233")
+//john.displayFullName();
+//john.displayNIM();
+//student.halo();
+
+//SETER & GETER
+
+//class student extends Person{
+//    constructor(firstName, lastName,NIM, age){
+//        super(firstName, LastName);
+//        this.NIM = NIM;
+//        this.age = age;
+//    }
+//    static halo(){// static dianggap bukan function, properti didalamnya tidak bisa dipakai
+//        console.log (`halo `);
+//    }
+//    displayNIM()
+//    {
+//        console.log (`Your NIM is $(this,NIM)`);
+//    }
+//    set studentScore(score){ //seter
+//        this.score = score;
+//    }
+//    get studentScore()//geter
+//    {return this.score;
+//    }
+//}
+//const john = new Student ("John", "Doe", "112233")
+//john.displayFullName();
+//john.displayNIM();
+//student.halo(); 
+//john.studentScore =90;
+//console.log (john.studentScore);
