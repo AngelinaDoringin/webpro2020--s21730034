ef00132add4e271a2ed1e33d5ceb8fd95c93c655
AngelinaDoringin <s21730034@student.unklab.ac.id>
2020-11-12 22:41:49 +0700

    add pertemuan 21, 22

---
+++ pertemuan_20 exercise04/src/Utils/person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name: "Angel",
+        institution: "Unklab",
+        address: "Timika",
+        phoneNumber: "123",
+    },
+    {
+        name: "Vabiola",
+        institution: "Unklab",
+        address: "Manado",
+        phoneNumber: "456",
+    },
+    {
+        name: "Fabby",
+        institution: "Unklab",
+        address: "Tompaso",
+        phoneNumber: "789",
+    },
+    {
+        name: "Eng",
+        institution: "Unklab",
+        address: "Mogoyunggung",
+        phoneNumber: "1012",
+    },
+];
