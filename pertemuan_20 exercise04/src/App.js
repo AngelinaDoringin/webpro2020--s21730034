5c297fe75ea5d22fe9fe077fedb3409dd213570a
AngelinaDoringin <s21730034@student.unklab.ac.id>
2020-12-03 15:52:23 +0700

    add pertemuan 25

--- pertemuan_20 exercise04/src/App.js
+++ pertemuan_20 exercise04/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 // import Headerr from './Components/Headerr';
 // import Hellow from './Components/Hellow';
 // import Content from './Components/Content';
@@ -18,7 +18,8 @@
 // }
 // export default App;
 // 
-  
+ 
+/*
 import React from 'react';
 import './App.css';
 const sortByPower = (a, b) => {
@@ -68,6 +69,7 @@ const App = () => {
   );
 }
 export default App;
+*/
 
 /*
 Pertemuan 23 no class
@@ -111,3 +113,43 @@ class App extends Component{
 }
   export default App;
 */ 
+
+
+// Pertemuan 25 //
+import React from "react";
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
+import { Home, Login, Register } from "./pages";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>  
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
+  )
+}
